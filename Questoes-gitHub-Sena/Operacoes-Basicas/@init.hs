6efe93536d7fb0401e225260abf29764f73d1413
Eduardo Tercio <dudubezerra33@hotmail.com>
2022-04-04 21:55:18 -0300

    @init melhorado

--- Questoes-gitHub-Sena/Operacoes-Basicas/@init.hs
+++ Questoes-gitHub-Sena/Operacoes-Basicas/@init.hs
@@ -3,10 +3,7 @@ main = do
     a <- readLn :: IO [Int]
     print $ primeiros a
 
-getInit :: [Int] -> [Int] -> [Int]
-getInit xs [] = xs
-getInit xs (_:[]) = xs
-getInit xs (y:ys) = xs ++ [y] ++ (getInit xs ys)
-
 primeiros :: [Int] -> [Int]
-primeiros xs = getInit [] xs
+primeiros [] = []
+primeiros (_:[]) = []
+primeiros (x:xs) = [x] ++ primeiros xs
