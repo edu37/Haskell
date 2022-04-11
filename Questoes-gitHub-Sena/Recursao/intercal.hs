84866635d880fe8c33640625316b51c47304e840
Eduardo Tercio <dudubezerra33@hotmail.com>
2022-04-11 18:21:48 -0300

    (intercal) Resolvido

---
+++ Questoes-gitHub-Sena/Recursao/intercal.hs
@@ -0,0 +1,11 @@
+
+intercal :: (Eq a) => [a] -> [a] -> [a]
+intercal [] ys = ys
+intercal xs [] = xs
+intercal (x:xs) (y:ys) = x:y:(intercal xs ys)
+
+main :: IO()
+main = do
+    a <- readLn :: IO [Int]
+    b <- readLn :: IO [Int]
+    print $ intercal a b
